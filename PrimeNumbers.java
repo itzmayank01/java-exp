789d5c5ba76d6475cd5ab73e9edf3f878ad03c29
Mayank Thakur <146236883+itzmayank01@users.noreply.github.com>
2025-01-29 11:33:35 +0530

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,24 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 2; i <= 1000; i++) {
+            if (isPrime(i)) {
+                count++;
+                System.out.println(i);
+            }
+        }
+        System.out.println("Total number of prime numbers between 1 and 1000: " + count);
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
